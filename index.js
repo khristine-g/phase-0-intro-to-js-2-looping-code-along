29112465c513ca09bc91608d9588419d255dae2c
khristine-g <githigekhristine@gmail.com>
2023-03-18 22:32:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+
+
+  function writeCards(names, gift) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${gift} gift!`;
+      messages.push(message);
+    }
+    return messages;
+  }
+  
+  
+  
+  
+  
+  
